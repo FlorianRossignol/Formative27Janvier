7a2b71a71c26239c84a0f175305242e2ba356074
FlorianRossignol <rossignol.florian.fr@gmail.com>
2021-01-25 15:27:25 +0100

    add script and player characterscript

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer PlayerCharacterSprite_;
+    [SerializeField] Rigidbody2D BodyPlayerCharacter_;
+    private Transform PlayerCharcterTransform_;
+    private const float DeadZone_ = 0.1f;
+    private const float MoveSpeed_ = 2.0f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        BodyPlayerCharacter_.velocity = new Vector2(Input.GetAxis("Horizontal") * MoveSpeed_, BodyPlayerCharacter_.velocity.y);
+    }
+}
