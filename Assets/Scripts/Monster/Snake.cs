fb6c8b81d2ad72beccc3e2a6f6cbdc4e0a722a21
Florian Rossignol <rossignol.florian.fr@gmail.com>
2021-01-26 17:25:05 +0100

    add script for snake

---
+++ Assets/Scripts/Monster/Snake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    [SerializeField] Animator snakeanim_;
+    private State currentstate_;
+    private enum State
+    {
+        NONE,
+        IDLE
+    }
+    
+    void Start()
+    {
+        currentstate_ = State.IDLE;
+    }
+
+   
+    void FixedUpdate()
+    {
+
+    }
+
+    void ChangeState(State state)
+    {
+        switch (state)
+        {
+            case State.IDLE:
+                snakeanim_.Play("idlesnake");
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException(nameof(state), state, null);
+        }
+        currentstate_ = state;
+
+    }
+}
