a7dabc493ffea12d0639fdb2c57c59b7911ff0c0
FlorianRossignol <rossignol.florian.fr@gmail.com>
2021-01-26 09:02:36 +0100

    work anim

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -31,9 +31,7 @@ public class PlayerCharacter : MonoBehaviour
     void Update()
     {
         bodyPlayerCharacter_.velocity = new Vector2(Input.GetAxis("Horizontal") * MoveSpeed, bodyPlayerCharacter_.velocity.y);
-        bodyPlayerCharacter_.velocity = new Vector2(bodyPlayerCharacter_.velocity.x,Input.GetAxis("Vertical") * MoveSpeed);
-
-       
+        bodyPlayerCharacter_.velocity = new Vector2(bodyPlayerCharacter_.velocity.x, Input.GetAxis("Vertical") * MoveSpeed);
     }
 
 
