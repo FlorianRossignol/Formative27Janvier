e875b5614f26f67b0651714da8b2c2d639d434ff
FlorianRossignol <rossignol.florian.fr@gmail.com>
2021-01-26 08:42:23 +0100

    work anim

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class PlayerCharacter : MonoBehaviour
     private enum State
     {
         NONE,
+        IDLE,
         WALKSIDE,
         WALKDOWN,
         WALKUP
@@ -16,7 +18,8 @@ public class PlayerCharacter : MonoBehaviour
     [SerializeField] private Animator anim_;
     
     private Transform playerCharacterTransform_;
-    private bool facingRight_;
+    private State currentstate_;
+    private bool facingRight_ = false;
     private const float DeadZone = 0.1f;
     private const float MoveSpeed = 2.0f;
     void Start()
@@ -29,5 +32,37 @@ public class PlayerCharacter : MonoBehaviour
     {
         bodyPlayerCharacter_.velocity = new Vector2(Input.GetAxis("Horizontal") * MoveSpeed, bodyPlayerCharacter_.velocity.y);
         bodyPlayerCharacter_.velocity = new Vector2(bodyPlayerCharacter_.velocity.x,Input.GetAxis("Vertical") * MoveSpeed);
+
+       
+    }
+
+
+    void Flip()
+    {
+        playerCharacterSprite_.flipX = !playerCharacterSprite_.flipX;
+        facingRight_ = !facingRight_;
+    }
+
+
+    void ChangeState(State state)
+    {
+        switch(state)
+        {
+            case State.IDLE:
+                anim_.Play("idle");
+                break;
+            case State.WALKSIDE:
+                anim_.Play("walkleftright");
+                break;
+            case State.WALKDOWN:
+                anim_.Play("walkdown");
+                break;
+            case State.WALKUP:
+                anim_.Play("walkup");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(state), state, null);
+        }
+        currentstate_ = state;
     }
 }
